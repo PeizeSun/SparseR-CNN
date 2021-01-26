7e4acb6a02e53fd083dfeeca0bc35ca285fc5183
ShoufaChen <shoufachen@163.com>
2021-01-26 11:20:54 +0800

    robust focal loss when alpah=-1
    `alpha`: (optional) Weighting factor in range (0,1) to balance
    positive vs negative examples. Default = -1 (no weighting).

--- projects/SparseRCNN/sparsercnn/loss.py
+++ projects/SparseRCNN/sparsercnn/loss.py
@@ -246,8 +246,11 @@ class HungarianMatcher(nn.Module):
             # Compute the classification cost.
             alpha = self.focal_loss_alpha
             gamma = self.focal_loss_gamma
-            neg_cost_class = (1 - alpha) * (out_prob ** gamma) * (-(1 - out_prob + 1e-8).log())
-            pos_cost_class = alpha * ((1 - out_prob) ** gamma) * (-(out_prob + 1e-8).log())
+            neg_cost_class = (out_prob ** gamma) * (-(1 - out_prob + 1e-8).log())
+            pos_cost_class = ((1 - out_prob) ** gamma) * (-(out_prob + 1e-8).log())
+            if alpha >= 0:
+               neg_cost_class *= (1 - alpha)
+               pos_cost_class *= alpha
             cost_class = pos_cost_class[:, tgt_ids] - neg_cost_class[:, tgt_ids]
         else:
             cost_class = -out_prob[:, tgt_ids]
